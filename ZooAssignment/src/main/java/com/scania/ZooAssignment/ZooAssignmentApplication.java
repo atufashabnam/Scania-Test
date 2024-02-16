2b6090c52a2fb884d36d8b009336e3634aa2809e
Atufa Shabnam <atufa.shabnam@gmail.com>
2024-02-16 18:00:47 +0100

    feat: springboot project created for backend

---
+++ ZooAssignment/src/main/java/com/scania/ZooAssignment/ZooAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.scania.ZooAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooAssignmentApplication.class, args);
+	}
+
+}
---
+++ ZooAssignment/src/test/java/com/scania/ZooAssignment/ZooAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scania.ZooAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
