077e99dea7bd9912a8b0f6419a0b5be6e6dd7695
Atufa Shabnam <atufa.shabnam@gmail.com>
2024-02-17 11:44:04 +0100

    feat: implemented cost calcution of food price per day

--- ZooAssignment/src/main/java/com/scania/ZooAssignment/service/ZooServiceImpl.java
+++ ZooAssignment/src/main/java/com/scania/ZooAssignment/service/ZooServiceImpl.java
@@ -1,70 +1,86 @@
 package com.scania.ZooAssignment.service;
 
-import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.exceptions.CsvException;
+import com.scania.ZooAssignment.model.Animal;
 import com.scania.ZooAssignment.model.AnimalDiet;
 import com.scania.ZooAssignment.model.Zoo;
-import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Unmarshaller;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
-public class ZooServiceImpl implements ZooService{
+public class ZooServiceImpl implements ZooService {
 
-    private final ResourceLoader resourceLoader;
+    private FileService fileService;
 
-    public ZooServiceImpl(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
+    public ZooServiceImpl(FileService fileService) {
+        this.fileService = fileService;
     }
-
 
     @Override
-    public double calculateCost() throws IOException, CsvException, JAXBException {
-        Map<String, Float> prices = loadPricesFromFile();
-        List<AnimalDiet> diets = populateAnimalDietFromCsv();
-        Zoo zooAnimalList = populateZooAnimalsFromXml();
-        return 0;
+    public Float calculateZooCostPerDay() throws IOException, CsvException, JAXBException {
+        Map<String, Float> prices = fileService.loadPricesFromFile();
+        List<AnimalDiet> diets = fileService.populateAnimalDietFromCsv();
+        Zoo zooAnimalList = fileService.populateZooAnimalsFromXml();
+        Map<String, AnimalDiet> animalDietMap = convertAnimalDietToMap(diets);
+        return calculateCost(prices, animalDietMap, zooAnimalList);
     }
 
-    private  Map<String, Float> loadPricesFromFile() throws IOException{
-        Resource resource =resourceLoader.getResource("classpath:" + "prices.txt");
-        InputStream inputStream = resource.getInputStream();
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            return reader.lines()
-                    .map(line -> line.split("\\s*=\\s*", 2))
-                    .collect(Collectors.toMap(
-                            data -> data[0],
-                            data -> Float.parseFloat(data[1])
-                    ));
-        } catch(Exception ex) {
-            throw new IOException("Exceptionwhile processing Price file");
+    private Map<String, AnimalDiet> convertAnimalDietToMap(List<AnimalDiet> diets) {
+        Map<String, AnimalDiet> animalDietMap = diets.stream().collect(Collectors.toMap(
+                animal -> animal.getType(), animal -> animal));
+        return animalDietMap;
+    }
+
+    private Float calculateCost(Map<String, Float> prices, Map<String, AnimalDiet> animalDietMap, Zoo zooAnimals) {
+        Float costForLion = iterateZooAnimalsAndCalculateCost(prices, animalDietMap.get("Lion"), zooAnimals.getLions());
+        Float costForGiraffe = iterateZooAnimalsAndCalculateCost(prices, animalDietMap.get("Giraffe"),
+                zooAnimals.getGiraffes());
+        Float costForTiger = iterateZooAnimalsAndCalculateCost(prices, animalDietMap.get("Tiger"),
+                zooAnimals.getTigers());
+        Float costForZebra = iterateZooAnimalsAndCalculateCost(prices, animalDietMap.get("Zebra"),
+                zooAnimals.getZebras());
+        Float costForWolf = iterateZooAnimalsAndCalculateCost(prices, animalDietMap.get("Wolf"),
+                zooAnimals.getWolves());
+        Float costForPiranha = iterateZooAnimalsAndCalculateCost(prices, animalDietMap.get("Piranha"),
+                zooAnimals.getPiranhas());
+        return costForLion + costForGiraffe + costForTiger + costForZebra + costForWolf + costForPiranha;
+    }
+
+    private Float iterateZooAnimalsAndCalculateCost(Map<String, Float> prices, AnimalDiet diet, List<Animal> animals) {
+        Float totalCost = 0F;
+        for (Animal animal : animals) {
+            if (diet != null) {
+                Float foodPerWeight = diet.getFoodPerWeight();
+                String foodType = diet.getFoodType();
+                Float percentage = parsePercentage(diet.getPercentageOfMeat());
+                if (foodType.equals("both")) {
+                    Float fruitFoodPrice = prices.get("fruit");
+                    Float meatFoodPrice = prices.get("meat");
+                    Float bodyWeightForMeat = animal.getKg() * percentage;
+                    Float bodyWeightForFruit = animal.getKg() - bodyWeightForMeat;
+                    totalCost += meatFoodPrice * bodyWeightForMeat * foodPerWeight;
+                    totalCost += fruitFoodPrice * bodyWeightForFruit * foodPerWeight;
+
+                } else {
+                    Float foodPrice = prices.get(foodType);
+                    totalCost += foodPrice * animal.getKg() * foodPerWeight;
+                }
+            }
+        }
+        return totalCost;
+    }
+
+    private Float parsePercentage(String percentage) {
+        if (percentage.equals("")) {
+            return 1F;
+        } else {
+            return Float.parseFloat(percentage.replace("%", "")) / 100F;
         }
     }
 
-    private List<AnimalDiet> populateAnimalDietFromCsv() throws IOException, CsvException {
-        Resource resource = resourceLoader.getResource("classpath:" + "animals.csv");
-        return new CsvToBeanBuilder<AnimalDiet>(new InputStreamReader(resource.getInputStream()))
-                .withType(AnimalDiet.class)
-                .withSeparator(';')
-                .build()
-                .parse();
-    }
-
-    private Zoo populateZooAnimalsFromXml() throws IOException, JAXBException {
-        Resource resource = resourceLoader.getResource("classpath:" + "zoo.xml");
-        JAXBContext jaxbContext = JAXBContext.newInstance(Zoo.class);
-        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-        return (Zoo)jaxbUnmarshaller.unmarshal(new InputStreamReader(resource.getInputStream()));
-    }
 }
